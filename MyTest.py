21147d541f8b7bf447b53e2cfe99ac9e1446dd5a
rascarf <459885351@qq.com>
2021-04-16 14:21:38 +0800

    Refactor home page view to use a template

--- MyTest.py
+++ MyTest.py
@@ -1,5 +1,33 @@
 from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
-assert 'To-Do lists' in browser.title
+class mytest(unittest.TestCase):
+    browser=webdriver.Chrome()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+    # Edith has heard about a cool new online to-do app.
+        self.browser.get('http://localhost:8000')
+    #She notices the page title and header mention to-do lists
+        self.assertIn("To-Do",self.browser.title)
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do",header_text)
+    #She is invited to enter a to_do item straight away
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(
+        inputbox.get_attribute("placeholder"),
+        "Enter a to-do item"
+        )
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id("id_list_table")
+        rows=table.fin_element_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1:Buy peacock feathers" for row in rows)
+        )
+        self.fail("Finish the test!")
+
+if __name__=="__main__":
+        mytest().test_can_start_a_list_and_retrieve_it_later()
+
--- lists/tests.py
+++ lists/tests.py
@@ -2,19 +2,12 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page # (2)
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve("/") #(1)
-        self.assertEqual(found.func,home_page) #(1)
-
-    def test_home_page_returns_correct_html(self):
-        request=HttpRequest()
-        response =home_page(request)
-        html =response.content.decode("utf-8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>",html)
-        self.assertTrue(html.endswith("</html>"))
+    def test_uses_home_template(self):
+        response =self.client.get("/")
+        self.assertTemplateUsed(response,'home.html')
 
