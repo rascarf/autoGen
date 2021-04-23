d0dd40968d7b4e892700622aef611f6f9094664b
rascarf <459885351@qq.com>
2021-04-23 14:21:10 +0800

    2021-4-23

--- MyTest.py
+++ MyTest.py
@@ -15,18 +15,27 @@ class mytest(unittest.TestCase):
         self.assertIn("To-Do",header_text)
     #She is invited to enter a to_do item straight away
         inputbox = self.browser.find_element_by_id("id_new_item")
-        self.assertEqual(
-        inputbox.get_attribute("placeholder"),
-        "Enter a to-do item"
-        )
+        inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+
         table = self.browser.find_element_by_id("id_list_table")
-        rows=table.fin_element_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "1:Buy peacock feathers" for row in rows)
+        rows=table.find_elements_by_tag_name("tr")
+
+        self.assertIn("1:Buy peacock feathers",[row.text for row in rows])
+        self.assertIn(
+            '2:Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
         self.fail("Finish the test!")
+    '''
+        self.assertTrue(
+            any(row.text == "1:Buy peacock feathers" for row in rows),
+            f"New to-do item did not appear in table. Contents were :\n{table.text}"
+        )
+    '''
+
+
 
 if __name__=="__main__":
         mytest().test_can_start_a_list_and_retrieve_it_later()
