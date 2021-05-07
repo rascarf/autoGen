2b012088109e6e8aaca528085d1396e7283623ae
rascarf <459885351@qq.com>
2021-05-07 14:33:29 +0800

    The 5th chapter has done! wowgit add .!

--- MyTest.py
+++ MyTest.py
@@ -6,6 +6,11 @@ import unittest
 class mytest(unittest.TestCase):
     browser=webdriver.Chrome()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
     # Edith has heard about a cool new online to-do app.
         self.browser.get('http://localhost:8000')
@@ -13,27 +18,26 @@ class mytest(unittest.TestCase):
         self.assertIn("To-Do",self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn("To-Do",header_text)
+
     #She is invited to enter a to_do item straight away
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows=table.find_elements_by_tag_name("tr")
+        #self.check_for_row_in_list_table("1:Buy peacock feathers")
+        #table = self.browser.find_element_by_id("id_list_table")
+        #rows=table.find_elements_by_tag_name("tr")
 
-        self.assertIn("1:Buy peacock feathers",[row.text for row in rows])
-        self.assertIn(
-            '2:Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table("1:Buy peacock feathers")
+        self.check_for_row_in_list_table("2:Use peacock feathers to make a fly")
+
         self.fail("Finish the test!")
-    '''
-        self.assertTrue(
-            any(row.text == "1:Buy peacock feathers" for row in rows),
-            f"New to-do item did not appear in table. Contents were :\n{table.text}"
-        )
-    '''
 
 
 
